fd573f8dd3457452792e1804829851ed57c663c5
Olekszij <olekszijk@gmail.com>
2026-07-25 01:05:30 +0200

    bla bla bla

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,4 +1,21 @@
 const serverless = require('serverless-http');
 const app = require('../../app');
 
-exports.handler = serverless(app);
+const handler = serverless(app);
+
+exports.handler = async (event, context) => {
+  // Handle CORS
+  const response = await handler(event, context);
+  
+  if (event.httpMethod === 'POST') {
+    response.headers = {
+      ...response.headers,
+      'Content-Type': 'text/html; charset=utf-8',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    };
+  }
+  
+  return response;
+};
